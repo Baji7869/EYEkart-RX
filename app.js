a079027c2992f23aa3b313ad71d89c130bf19995
Baji7869 <158423037+Baji7869@users.noreply.github.com>
2026-02-12 20:59:53 +0530

    Refactor downloadPDF to use print instead of PDF
    Refactored downloadPDF function to simplify PDF generation and added print functionality.

--- app.js
+++ app.js
@@ -49,51 +49,13 @@ function generate() {
 
 function downloadPDF() {
 
-    const { jsPDF } = window.jspdf;
-    const doc = new jsPDF();
+    const name =
+        document.getElementById("pName").innerText.replace(/\s+/g, "_");
 
-    const name = document.getElementById("pName").innerText;
-    const date = document.getElementById("pDate").innerText;
+    const date =
+        document.getElementById("pDate").innerText.replace(/[\s,]+/g, "_");
 
-    const rSphere = document.getElementById("prSphere").innerText;
-    const lSphere = document.getElementById("plSphere").innerText;
+    document.title = `${name}_${date}`;
 
-    const rCyl = document.getElementById("prCyl").innerText;
-    const lCyl = document.getElementById("plCyl").innerText;
-
-    const rAxis = document.getElementById("prAxis").innerText;
-    const lAxis = document.getElementById("plAxis").innerText;
-
-    const rPd = document.getElementById("prPd").innerText;
-    const lPd = document.getElementById("plPd").innerText;
-
-    const testedBy = document.getElementById("pTestedBy").innerText;
-
-    // Header
-    doc.setFontSize(18);
-    doc.text("Sarkar Eye Camps and Optics", 20, 20);
-
-    doc.setFontSize(14);
-    doc.text("Eye Test Prescription", 20, 30);
-
-    // Name + Date
-    doc.setFontSize(12);
-    doc.text(`Patient Name: ${name}`, 20, 45);
-    doc.text(`Date: ${date}`, 20, 55);
-
-    // Table Data
-    doc.text("Right Eye        Left Eye", 20, 75);
-
-    doc.text(`Spherical:   ${rSphere}            ${lSphere}`, 20, 90);
-    doc.text(`Cylinder:    ${rCyl}            ${lCyl}`, 20, 100);
-    doc.text(`Axis:        ${rAxis}            ${lAxis}`, 20, 110);
-    doc.text(`PD:          ${rPd}            ${lPd}`, 20, 120);
-
-    doc.text(`Tested By: ${testedBy}`, 20, 140);
-
-    const fileName =
-        name.replace(/\s+/g, "_") + "_" +
-        date.replace(/[\s,]+/g, "_") + ".pdf";
-
-    doc.save(fileName);
+    window.print();
 }
